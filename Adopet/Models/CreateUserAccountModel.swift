696874933823e18e9e8f95301161e1405573bc38
Guilhermemsoares <guigamsoares@hotmail.com>
2026-05-16 13:24:41 -0300

    Initial commit

---
+++ Adopet/Models/CreateUserAccountModel.swift
@@ -0,0 +1,13 @@
+//
+//  CreateUserAccountModel.swift
+//  Adopet
+//
+//  Created by Guilherme Moreira Soares on 15/05/26.
+//
+
+struct CreateUserAccountModel {
+    let name: String
+    let email: String
+    let phoneNumber: String
+    let password: String
+}
